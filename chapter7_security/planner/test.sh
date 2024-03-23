555ed62b2f95a8698a60b75472b068a75bd32c57
ksm463 <ksm46351@gmail.com>
2024-03-23 20:05:12 +0900

    create_chapter7

---
+++ chapter7_security/planner/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -X 'POST' \
+  'http://127.0.0.1:8000/user/signup' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{"email": "reader@packt.com","password": "examplary","events": []}'
