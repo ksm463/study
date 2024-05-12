64b994b7fe186502fe7b0b776daa7894e224545f
ksm463 <ksm46351@gmail.com>
2024-05-12 18:03:44 +0900

    delete_copied_files

--- 01.web-FastAPI/README.md
+++
@@ -1,6 +0,0 @@
-# web-FastAPI
-studying FastAPI book record</br>
-</br>
-![B9703548802_l](https://github.com/ksm463/web-FastAPI/assets/113885176/70add5df-2e75-4b07-a919-9c191bf59bd0)
-
-https://github.com/hanbit/web-with-fastapi?tab=readme-ov-file
--- 01.web-FastAPI/chapter7_security/planner/event.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -X 'DELETE' \
-  'http://127.0.0.1:8000/event/6600387c3c0afb880ce9e7aa' \
-  -H 'accept: application/json' \
-  -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoicmVhZGVyQHBhY2t0LmNvbSIsImV4cGlyZXMiOjE3MTEyOTM4MTEuODc0OTYzOH0.7nwqrT8wISIzaHlhKkvEOp2ScK0L959qGasRmlyeO6c' \
